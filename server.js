092631a6c2be1c8c7a69a4977fae1d905b91437f
Militao <Militao@DESKTOP-9FVCTRK>
2019-11-03 14:52:58 -0300

    Server for FrontEnd.

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express'),
+    path = require('path');
+
+const app = express();
+
+app.use(express.static('./dist/frontend-test'));
+
+app.get('/*', (req, res) => {
+    res.sendFile(path.join(__dirname, '/dist/frontend-test/index.html'));
+});
+
+app.listen(process.env.PORT || 8080, ()=> {
+    console.log('Server started');
+})
